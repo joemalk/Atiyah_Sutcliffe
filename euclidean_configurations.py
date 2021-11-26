fb3e4c8d82163984b8a3324a41cd7dcecd081cd2
joemalk <20187639+joemalk@users.noreply.github.com>
2021-11-25 19:45:08 -0500

    perturbed the configuration set in case of some vertical directions before taking AS_det.

--- euclidean_configurations.py
+++ euclidean_configurations.py
@@ -1,5 +1,6 @@
 # Import the necessary modules/libraries
 import numpy as np
+from scipy.linalg import expm
 
 class EuclideanConfigurationSet:
     """
@@ -24,36 +25,74 @@ class EuclideanConfigurationSet:
             return (len(confs.shape) == 3) & (confs.shape[-1] == 3)
 
 
-    def AS_dets(self): #TODO: make this method able to handle confs with vertical directions
+    def has_distinct_points(self):
+        """
+        Check if all points within a configuration are distinct
+        :return: bool
+        """
+        for i in range(self.n - 1):
+            for j in range(i + 1, self.n):
+                xs_ij = -self.confs[:, i, :] + self.confs[:, j, :]
+                xs_ij.reshape(self.N,3)
+                if (xs_ij == [0., 0., 0.]).all(axis=-1).any():
+                    return False
+        return True
+
+
+    def AS_dets(self):
         """
         Return the AS det of the EuclideanConfiguration object
         :return: float
         """
+        if not self.has_distinct_points():
+            raise Exception("Error: one of the configurations does not have distinct points.")
+
+        confs = self.confs.copy()
+
+        def has_vertical_directions(confs):
+            """
+            Check for vertical directions (parallel to the z-axis)
+            :return: bool
+            """
+            for i in range(self.n - 1):
+                for j in range(i + 1, self.n):
+                    xs_ij = -confs[:, i, :] + confs[:, j, :]
+                    if (xs_ij[:, :2] == [0., 0.]).all(axis=-1).any():
+                        return True
+            return False
+
+        while has_vertical_directions(confs):
+            eps = np.random.random()
+            inf_rot = np.array([[0.,0.,-1.],[0.,0.,0.],[1.,0.,0.]])
+            rot = expm(eps * inf_rot)
+            confs = confs.dot(rot)
+
+
         # compute j of a vector in C2
         def quatj(u):
             return np.concatenate([-u[:,1:2].conjugate(), u[:,0:1].conjugate()], axis= -1)
 
+
         # Compute the Hopf lifts of all pairwise directions
-        def hopf_lifts():
+        def hopf_lifts(confs):
             hl = np.zeros((self.N, self.n, self.n - 1, 2), dtype=np.complex)
 
             for i in range(self.n - 1):
                 for j in range(i+1, self.n):
-                    xs_ij = -self.confs[:, i, :] + self.confs[:, j, :]
+                    xs_ij = -confs[:, i, :] + confs[:, j, :]
                     rs_ij = np.linalg.norm(xs_ij, axis=-1).reshape(-1,1)
                     if (xs_ij[:, 2] != rs_ij[:]).all():
-                        hopf_lift = np.concatenate([np.ones((self.N,1), dtype=complex), (xs_ij[:, 0:1] + 1.j * xs_ij[:, 1:2]) / (rs_ij - xs_ij[:, 2:3])],\
+                        hopf_lift = np.concatenate([np.ones((self.N,1), dtype=complex), \
+                                                    (xs_ij[:, 0:1] + 1.j * xs_ij[:, 1:2]) / (rs_ij - xs_ij[:, 2:3])],\
                                                    axis=-1)
                         hl[:, i, j-1, :] = hopf_lift / np.linalg.norm(hopf_lift, axis=-1).reshape(-1,1)
                     else:
-                        pass #TODO: implement this case
-                        # hl[k, i, j, :] = [0., -1.]
-                        # norms[k, i, j] = 1.
+                        raise Exception("Error: division by 0 due to a vertical direction!")
                     hl[:, j, i, :] = quatj(hl[:, i, j-1, :])
             return hl
 
-        # Calculate the Hopf lifts of self.conf
-        hl = hopf_lifts()
+        # Calculate the Hopf lifts of confs
+        hl = hopf_lifts(confs)
 
         def poly_mul(polys_arr, linear_arr):
             N1,n1,d1 = polys_arr.shape
--- test_euclidean_configurations.py
+++ test_euclidean_configurations.py
@@ -22,3 +22,12 @@ euc_confs = EuclideanConfigurationSet(confs)
 print(f'The minimal real part of this sample is: {min(np.real(euc_confs.AS_dets()))}')
 T2_t1 = perf_counter()
 print(f'Test 2 completed in {T2_t1-T2_t0} seconds.')
+
+print("Test 3: Compute AS_det for configurations with some (or all) vertical directions.")
+T3_t0 = perf_counter()
+n = 4
+col_conf = np.array([[0.,0.,float(k)] for k in range(n)])
+euc_col_conf = EuclideanConfiguration(col_conf)
+print(f'Collinear configuration along z axis with n = {n} has AS det = {euc_col_conf.AS_det()}')
+T3_t1 = perf_counter()
+print(f'Test 3 completed in {T3_t1-T3_t0} seconds.')
